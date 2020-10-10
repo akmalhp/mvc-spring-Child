99186da6ec62bcc7e33203a9111ad824f20edd62
Akmal Prasetyo <akmal.prasetyo@gmail.com>
2020-10-10 11:36:16 +0700

    Update JisohController.java

--- src/main/java/com/jisoh/keybinding/JisohController.java
+++ src/main/java/com/jisoh/keybinding/JisohController.java
@@ -32,37 +32,10 @@ public class JisohController {
 
         model.addAttribute("form", booksForm);
         model.addAttribute("books", bookService.findAll());
-//        var a = resultService.findAll();
-//        gausah
-//        model.addAttribute("results", a.getSimilarData);
 
         return "main";
     }
-
-//    @GetMapping(value = "/create")
-//    public String showCreateForm(Model model) {
-//	System.out.println("aaaa");
-//        BooksCreationDto booksForm = new BooksCreationDto();
-//
-//        for (int i = 1; i <= 3; i++) {
-//            booksForm.addBook(new PrimaryKey());
-//        }
-//
-//        model.addAttribute("form", booksForm);
-//
-//        return "main";
-//    }
-
-	/*
-	 * @GetMapping(value = "/edit") public String showEditForm(Model model) {
-	 * List<PrimaryKey> books = new ArrayList<>(); bookService.findAll() .iterator()
-	 * .forEachRemaining(books::add);
-	 *
-	 * model.addAttribute("form", new BooksCreationDto(books));
-	 *
-	 * return "editBooksForm"; }
-	 */
-
+	
     @PostMapping(value = "/save")
     public String saveBooks(@ModelAttribute BooksCreationDto books, Model model) {
 
